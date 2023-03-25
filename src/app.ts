f2d172acc7f48b79373c3a61dfc06d93fea049da
Yi Chun <ycWang0107@protonmail.com>
2023-03-25 23:19:35 +0800

    feat: install nodemon & ts-node

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app: Application = express()
 
 const port = 3000
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
